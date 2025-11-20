8d63827a2984303874a52de012e32d6e1e5b90ed
Wrong1234 <mahabur24cse@gmail.com>
2025-11-20 14:34:28 +0600

    first commit

---
+++ src/routes/index.js
@@ -0,0 +1,16 @@
+// ============================================
+// FILE: src/routes/index.js
+// ============================================
+
+import express from 'express';
+import authRoutes  from '../modules/auth/auth.routes.js';
+// import messageRoutes from "../components/messages/message.routes.js";
+// import userRoutes from './user.routes.js';
+// import chatRoutes from './chat.routes.js';
+
+const router = express.Router();
+
+router.use('/auth', authRoutes);
+// router.use('/messages', messageRoutes);
+
+export default router;
