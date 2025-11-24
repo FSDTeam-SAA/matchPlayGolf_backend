d3241986fc9d88ffdc85a66bf8ad30c223a9bd21
Rafiul-Hossain <rafiulhossain4393@email.com>
2025-11-24 11:05:19 +0600

    implement match management features including creation, retrieval, updating, and deletion of matches

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,12 +5,14 @@
 import express from 'express';
 import authRoutes  from '../modules/auth/auth.routes.js';
 import userRoutes from '../modules/user/user.routes.js';
+import matchRoutes from '../modules/match/match.routes.js';
 
 
 const router = express.Router();
 
 router.use('/auth', authRoutes);
 router.use('/user', userRoutes);
+router.use('/match', matchRoutes);
 
 
 export default router;
