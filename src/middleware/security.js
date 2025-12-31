630596f045523ce1f4091a1e883b2bc217eb9bb6
Wrong1234 <mahabur24cse@gmail.com>
2025-12-31 14:44:08 +0600

    update

--- src/middleware/security.js
+++ src/middleware/security.js
@@ -12,7 +12,7 @@ export const applyMiddleware = (app) => {
   
 
   app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "*",
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   }));
